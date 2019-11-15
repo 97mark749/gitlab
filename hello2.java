3e9ac066d6aefadd1750a512bdb094ea2dcc00ca
core.safecrif <foxb4@go.stockton.edu>
2019-11-15 15:25:24 -0500

    Collaborating

---
+++ hello2.java
@@ -0,0 +1,13 @@
+//This is a file added by Brian Fox
+public class hello2{
+
+public static void main(String argv[]){
+	
+	String name = "World";
+	if(argv.length !=){
+		name = argv[0];
+	}
+
+	System.out.println("Hello, " + name + "!");
+	}
+}
